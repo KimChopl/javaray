e0285247780064723472dad5f502678fc2dabe17
KimChopl <jsan08020@gmail.com>
2025-01-28 19:25:40 +0900

    25.01.28
    spring boot 초기 세팅 v.0

---
+++ back-workspace/javaray/src/main/java/com/kh/javaray/JavarayApplication.java
@@ -0,0 +1,13 @@
+package com.kh.javaray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavarayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavarayApplication.class, args);
+	}
+
+}
---
+++ back-workspace/javaray/src/test/java/com/kh/javaray/JavarayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.javaray;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavarayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
