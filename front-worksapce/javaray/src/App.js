f6c18e38125b21566f617d5d3792ddffeea9159b
지민 <149668162+jiminhaneee@users.noreply.github.com>
2025-02-05 11:36:12 +0900

    2/5 import 2개 있는게 있어서 제거

--- front-worksapce/javaray/src/App.js
+++ front-worksapce/javaray/src/App.js
@@ -1,6 +1,5 @@
 import Footer from "./Footer/Footer";
 import Shipping from "./shipping/Shipping";
-import { Routes, Route } from "react-router-dom";
 import ShippingDetail from "./shipping/shippingDetail/ShippingDetail";
 import { ModalProvider } from "./Modal/OpenOrCloseModal";
 import Modal from "./Modal/Modal";
@@ -11,12 +10,13 @@ function App() {
   return (
     <ModalProvider>
       <CarouselWithIndicatorsExample />
-
-      <Routes>
-        <Route path="shipping" element={<Shipping />} />
-        <Route path="/shipping/detail" element={<ShippingDetail />} />
-      </Routes>
-      <Footer />
+      <BrowserRouter>
+        <Routes>
+          <Route path="shipping" element={<Shipping />} />
+          <Route path="/shipping/detail" element={<ShippingDetail />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </ModalProvider>
   );
 }
