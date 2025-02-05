14734037279c4cc47a18eaffaae4870057a2ba4d
KimChopl <jsan08020@gmail.com>
2025-02-05 11:50:07 +0900

    25.02.05
    모달 수정

--- front-worksapce/javaray/src/Modal/OpenOrCloseModal.jsx
+++ front-worksapce/javaray/src/Modal/OpenOrCloseModal.jsx
@@ -2,18 +2,17 @@ import { createContext, useEffect, useState } from "react";
 
 export const ModalContext = createContext();
 
-export const ModalProvider = ({children}) => {
-    const [isModal, setIsModal] = useState(false);
-    useEffect(() => {
-        setIsModal(false)
-    }, [])
-    const openModal = (boolean) => {
-        setIsModal(boolean);
-    }
-    return(
-        <ModalContext.Provider value={{openModal, isModal}}>
-            {children}
-        </ModalContext.Provider>
-    )
-
-}
+export const ModalProvider = ({ children }) => {
+  const [isModal, setIsModal] = useState(false);
+  useEffect(() => {
+    setIsModal(isModal);
+  }, [isModal]);
+  const openModal = (boolean) => {
+    setIsModal(boolean);
+  };
+  return (
+    <ModalContext.Provider value={{ openModal, isModal, setIsModal }}>
+      {children}
+    </ModalContext.Provider>
+  );
+};
